36951040efffdd9d7361aa6d34369c82b20b88e3
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-02-16 12:09:06 -0800

    up and down

--- scaling-game/scripts/level.gd
+++ scaling-game/scripts/level.gd
@@ -14,7 +14,7 @@ func is_invalid(coords: Vector2i) -> bool:
 
 func _input(event):
 	if (event.is_action_pressed("up")):
-		var y: int = player.bounds.position.x
+		var y: int = player.bounds.position.y
 		var stopped: bool = false
 		while !stopped:
 			y -= 1
@@ -22,15 +22,24 @@ func _input(event):
 				if is_invalid(Vector2i(x, y)):
 					stopped = true
 		y += 1
-		if (y == player.bounds.position.x):
-			# shrink
-			pass
+		if (y == player.bounds.position.y):
+			player.scale("up", "contract", player.bounds.size.y - 1)
 		else:
-			pass
-		pass
+			player.scale("up", "expand", abs(player.bounds.position.y - y))
 	if (event.is_action_pressed("left")):
 		pass
 	if (event.is_action_pressed("right")):
 		pass
 	if (event.is_action_pressed("down")):
-		pass
+		var y: int = player.bounds.position.y + player.bounds.size.y
+		var stopped: bool = false
+		while !stopped:
+			y += 1
+			for x in range(player.bounds.position.x, player.bounds.position.x + player.bounds.size.x):
+				if is_invalid(Vector2i(x, y)):
+					stopped = true
+		y -= 1
+		if (y == player.bounds.position.y + player.bounds.size.y):
+			player.scale("down", "contract", player.bounds.size.y - 1)
+		else:
+			player.scale("down", "expand", abs(player.bounds.position.y + player.bounds.size.y - y))
