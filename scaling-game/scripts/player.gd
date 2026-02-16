221c86f7bfd284310ead3c2d4e8f9ff5085f61a1
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2026-02-16 11:22:19 -0800

    player work

--- scaling-game/scripts/player.gd
+++ scaling-game/scripts/player.gd
@@ -7,20 +7,34 @@ var rectangle : Rect2i
 
 
 func _ready() -> void:
-	rectangle = Rect2i(Vector2i(100, 100), Vector2i(150, 150))
+	rectangle = Rect2i(Vector2i(200, 200), Vector2i(400, 400))
+	#await get_tree().create_timer(0.5).timeout
+	#scale("right", "expand", 150)
+	#await get_tree().create_timer(0.5).timeout
+	#scale("down", "expand", 200)
+	#await get_tree().create_timer(0.5).timeout
+	#scale("left", "contract", 150)
 
 
 func _process(_delta: float) -> void:
 	color_rect.size = rectangle.size
 
 
-func scale(direction:String, amount:int):
+func scale(direction:String, type:String, amount:int):
 	match direction:
-		"left":
+		"left" when type == "expand":
 			rectangle.position.x -= amount
-		"right":
+		"left" when type == "contract":
+			rectangle.size.x -= amount
+		"right" when type == "expand":
+			rectangle.size.x += amount
+		"right" when type == "contract":
+			rectangle.position.x += amount
+		"up" when type == "expand":
+			rectangle.position.y -= amount
+		"up" when type == "contract":
+			rectangle.size.y -= amount
+		"down" when type == "expand":
 			rectangle.size.y += amount
-		"up":
-			rectangle.position.x -= amount
-		"down":
-			rectangle.size.y += amount
+		"down" when type == "contract":
+			rectangle.position.y -= amount
