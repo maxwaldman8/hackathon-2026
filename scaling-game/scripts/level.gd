ea349bce0f12fb1730cdc56bd42962294ebdfc39
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-02-16 11:40:20 -0800

    More level

--- scaling-game/scripts/level.gd
+++ scaling-game/scripts/level.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-@export var player: Node2D
+@export var player: Player
 @onready var grid: TileMapLayer = $Grid
 @export var level_bounds: Rect2i = Rect2i(0, 0, 0, 0)
 
-func square_has_wall(coords: Vector2i) -> bool:
-	grid.get_cell_source_id(coords)
-	
+func has_wall(coords: Vector2i) -> bool:
+	return grid.get_cell_source_id(coords) != -1
+
+func is_invalid(coords: Vector2i) -> bool:
+	return has_wall(coords) or !level_bounds.has_point(coords)
 
 func _input(event):
 	if (event.is_action_pressed("up")):
-		
+		for x in range(player.position.x, player.position.x + player.)
 		pass
 	if (event.is_action_pressed("left")):
 		pass
