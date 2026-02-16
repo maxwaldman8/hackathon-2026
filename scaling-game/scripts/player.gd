281e9481a3b1ef9bfd5633a2d8a223a4174a35d1
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2026-02-16 12:24:26 -0800

    tilemap in player

--- scaling-game/scripts/player.gd
+++ scaling-game/scripts/player.gd
@@ -2,13 +2,17 @@ class_name Player
 extends CharacterBody2D
 
 
+@export var level : Level
 @onready var color_rect = $ColorRect
 
 var bounds : Rect2i
+var grid : TileMapLayer
 
 
 func _ready() -> void:
-	bounds = Rect2i(Vector2i(200, 200), Vector2i(400, 400))
+	bounds = Rect2i(Vector2i(0, 0), Vector2i(1, 1))
+	grid = level.get_node("Grid")
+	print(grid)
 	#await get_tree().create_timer(0.5).timeout
 	#scale("right", "expand", 150)
 	#await get_tree().create_timer(0.5).timeout
@@ -18,7 +22,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	color_rect.size = bounds.size
+	color_rect.size = bounds.size * grid.tile_set.tile_size
 
 
 func scale(direction:String, type:String, amount:int):
