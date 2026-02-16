b5417f02071ad13fa35ba81ebb20ad2d4373b919
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-02-16 13:01:55 -0800

    Working!!

--- scaling-game/scripts/player.gd
+++ scaling-game/scripts/player.gd
@@ -27,17 +27,21 @@ func scale_bounds(direction:String, type:String, amount:int):
 	match direction:
 		"left" when type == "expand":
 			bounds.position.x -= amount
+			bounds.size.x += amount
 		"left" when type == "contract":
 			bounds.size.x -= amount
 		"right" when type == "expand":
 			bounds.size.x += amount
 		"right" when type == "contract":
 			bounds.position.x += amount
+			bounds.size.x = 1
 		"up" when type == "expand":
 			bounds.position.y -= amount
+			bounds.size.y += amount
 		"up" when type == "contract":
 			bounds.size.y -= amount
 		"down" when type == "expand":
 			bounds.size.y += amount
 		"down" when type == "contract":
-			bounds.position.y -= amount
+			bounds.position.y += amount
+			bounds.size.y = 1
